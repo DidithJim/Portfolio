b9ce7260275a7d777569fe4caf4952b7a56118bd
Judith Ríos <165655155+DidithJim@users.noreply.github.com>
2025-02-02 21:47:05 +0100

    Add files via upload
    Update Works Component

---
+++ DidithJim/src/components/Works.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import '../styles/projects.css'
+
+function Works () {
+    return(
+        <div className="project__page">
+            <main className="project__container">
+                <section className="project__section">
+                    <h1 className="project__title">Projects</h1>
+                    <div className="project__content">
+                        <div className="project__card">
+                            
+                        </div>
+                    </div>
+                </section>
+
+
+            </main>
+
+        </div>
+    )
+}
+
+export default Works;
