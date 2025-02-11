66dced73932e029abebe61cea1d536558b561101
Judith Ríos <165655155+DidithJim@users.noreply.github.com>
2025-02-11 11:19:24 +0100

    Update Home.jsx

--- DidithJim/src/pages/Home.jsx
+++ DidithJim/src/pages/Home.jsx
@@ -1,13 +1,31 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import '../styles/home.css'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
+import { MenuContext } from '../components/MenuContext'
+
 
 function Home() {
+    const { language } = useContext(MenuContext);
+    
+    const translations = {
+        EN: {
+            hero__text: 'hey! I\'m Didith <br /> I\'m a full stack developer <br /> and a ux/ui designer',
+            hero__link: 'projects',
+            hero__link2: 'about me'
+        },
+        ES: {
+            hero__text: '¡Hola! Soy Didith <br /> Soy desarrolladora full stack <br /> y diseñadora ux/ui',
+            hero__link: 'proyectos',
+            hero__link2: 'sobre mí'
+        }
+    }
+
+
     return(
         <div className='home__page'>
-        <Header />  
-        <Hero />
+            <Header />  
+            <Hero translations={translations} language={language} />
         </div>
     )
 }
