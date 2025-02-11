6f71fbad0dc1cdfa7290b162c8b6cac6b33679a8
Judith Ríos <165655155+DidithJim@users.noreply.github.com>
2025-02-11 21:43:12 +0100

    Update Header.jsx

--- DidithJim/src/components/Header.jsx
+++ DidithJim/src/components/Header.jsx
@@ -60,8 +60,8 @@ function Header() {
                                 loop={false}
                                 autoplay={false}
                                 style={{ 
-                                    width: 30,
-                                    height: 30,
+                                    width: 60,
+                                    height: 60,
                                     padding: 0,
                                     cursor: 'pointer',
                                 }}
