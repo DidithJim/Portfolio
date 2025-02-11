094aab91c17bcf842f9d183eb86788515d6a8fc7
Judith Ríos <165655155+DidithJim@users.noreply.github.com>
2025-02-11 11:18:30 +0100

    Update Header.jsx

--- DidithJim/src/components/Header.jsx
+++ DidithJim/src/components/Header.jsx
@@ -1,52 +1,71 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { Link } from 'react-router-dom';
-import UseAnimations from 'react-useanimations';
-import menu4 from 'react-useanimations/lib/menu4'
-
+import Lottie from 'lottie-react';
+import menuAnimation from '../assets/animation.json'
+import { MenuContext } from '../components/MenuContext'
 
 function Header() {
-    const [language, setLanguage] = useState('EN');
+    const { language, toggleLanguage } = useContext(MenuContext);
     const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-
-    const toggleLanguage = () => {
-        setLanguage(language === 'EN' ? 'ES' : 'EN');
-    };
-
+    const menuRef = React.useRef();
 
     const toggleMenu = () => {
         setIsMenuOpen(!isMenuOpen);
+        if (menuRef.current) {
+            if (!isMenuOpen) {
+                menuRef.current.setDirection(1);
+                menuRef.current.playSegments([0, 60], true);
+            } else {
+                menuRef.current.setDirection(-1);
+                menuRef.current.playSegments([60, 0], true);
+            }
+        }
         const menu = document.querySelector('.menu');
         const hero = document.querySelector('.hero');
+        const projectContainer = document.querySelector('.project__container');
+        const aboutContainer = document.querySelector('.about__container');
+        const contactContainer = document.querySelector('.contact__container');
        
         if (isMenuOpen) {
             menu.classList.remove('active');
             if (hero) hero.classList.remove('hidden');
+            if (projectContainer) projectContainer.classList.remove('hidden');
+            if (aboutContainer) aboutContainer.classList.remove('hidden');
+            if (contactContainer) contactContainer.classList.remove('hidden');
         } else {
             menu.classList.add('active');
             if (hero) hero.classList.add('hidden');
+            if (projectContainer) projectContainer.classList.add('hidden');
+            if (aboutContainer) aboutContainer.classList.add('hidden');
+            if (contactContainer) contactContainer.classList.add('hidden');
         }
     };
-
 
     return (
         <header className='header'>
             <nav className='header__nav'>
                 <div className='header__container'>
                     <Link to="/">
-                        <img className='header__logo' src="/img/logo-portfolio.png" alt="DidithJim Logo" />
+                        <img className='header__logo' src="/img/logo-porfolio.png" alt="DidithJim Logo" />
                     </Link>
                    
                     <div className='header__right-items'>
                         <button className='header__lang-btn' onClick={toggleLanguage}>
                             {language}
                         </button>
-                        <div className='header__menu-btn'>
-                            <UseAnimations
-                                animation={menu4}
-                                size={40}
-                                onClick={toggleMenu}
-                                reverse={isMenuOpen}
+                        <div className='header__menu-btn' onClick={toggleMenu}>
+                            <Lottie
+                                lottieRef={menuRef}
+                                animationData={menuAnimation}
+                                loop={false}
+                                autoplay={false}
+                                style={{ 
+                                    width: 120,
+                                    height: 120,
+                                    padding: 0,
+                                    cursor: 'pointer',
+                                }}
+                                initialSegment={[0, 30]}
                                 speed={1.5}
                             />
                         </div>
@@ -54,18 +73,16 @@ function Header() {
                 </div>
             </nav>
 
-
             <div className="menu">
                 <ul className="menu__list">
-                    <li><Link to="/"><span className='number'>01</span> HOME</Link></li>
-                    <li><Link to="/projects"><span className='number'>02</span> PROJECTS</Link></li>
-                    <li><Link to="/about"><span className='number'>03</span> ABOUT</Link></li>
-                    <li><Link to="/contact"><span className='number'>04</span> CONTACT</Link></li>
+                    <li><Link to="/"><span className='number'>01</span> {language === 'EN' ? 'HOME' : 'INICIO'}</Link></li>
+                    <li><Link to="/projects"><span className='number'>02</span> {language === 'EN' ? 'PROJECTS' : 'PROYECTOS'}</Link></li>
+                    <li><Link to="/about"><span className='number'>03</span> {language === 'EN' ? 'ABOUT' : 'SOBRE MÍ'}</Link></li>
+                    <li><Link to="/contact"><span className='number'>04</span> {language === 'EN' ? 'CONTACT' : 'CONTACTO'}</Link></li>
                 </ul>
             </div>
         </header>
     )
 }
 
-
-export default Header
+export default Header;
