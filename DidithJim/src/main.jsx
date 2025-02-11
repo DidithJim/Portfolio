413886ca91bcd70e99f4a1be5eda674d31158e17
Judith Ríos <165655155+DidithJim@users.noreply.github.com>
2025-02-11 11:22:13 +0100

    Update main.jsx

--- DidithJim/src/main.jsx
+++ DidithJim/src/main.jsx
@@ -6,17 +6,21 @@ import Home from './pages/Home'
 import Projects from './pages/Projects'
 import About from './pages/About'
 import Contact from './pages/Contact'
+import { MenuProvider } from './components/MenuContext'
 import './styles/home.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render( 
-     <Router>
-       <Routes>
-         <Route path="/" element={<Navigate to="/home" />} />
-         <Route path="/home" element={<Home />} />
-         <Route path="/projects" element={<Projects />} />
-         <Route path="/about" element={<About />} />
-         <Route path="/contact" element={<Contact />} />
-       </Routes>
-      </Router>
-  
+  ReactDOM.createRoot(document.getElementById('root')).render( 
+    <React.StrictMode>
+        <MenuProvider>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Navigate to="/home" />} />
+                    <Route path="/home" element={<Home />} />
+                    <Route path="/projects" element={<Projects />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/contact" element={<Contact />} />
+                </Routes>
+            </Router>
+        </MenuProvider>
+    </React.StrictMode>
 )
