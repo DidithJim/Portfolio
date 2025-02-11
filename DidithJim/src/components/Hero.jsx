a730131a89fc7bac43c52b9ed6f9702f8a09875c
Judith Ríos <165655155+DidithJim@users.noreply.github.com>
2025-02-11 11:18:51 +0100

    Update Hero.jsx

--- DidithJim/src/components/Hero.jsx
+++ DidithJim/src/components/Hero.jsx
@@ -2,24 +2,25 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/home.css';
 
-function Hero() {
+function Hero({ translations, language }) {
+    const text = translations[language];
+    
     return (
         <div className="hero">
             <div className='hero__container'>
-                <p className='hero__text'>
-                    hey! I'm Didith <br /> 
-                    I'm a full stack developer <br /> 
-                    and a ux/ui designer
+                <p className='hero__text' 
+                   dangerouslySetInnerHTML={{ __html: text.hero__text }}>
                 </p>
             </div>
             
             <div className="hero__info">
                 <ul className="hero__list">
                     <li className="hero__item">
-                        <Link to="/projects" className="hero__link">projects</Link>
+                        <Link to="/projects" className="hero__link">{text.hero__link}</Link>
                     </li>
                     <li className="hero__item">
-                        <Link to="/about" className="hero__link">about me</Link>
+                        <Link to="/about" className="hero__link">{text.hero__link2}</Link>
+
                     </li>
                 </ul>
             </div>
