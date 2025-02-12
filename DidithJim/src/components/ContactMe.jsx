d7e49db65849b56907c3b8a76d3c2fee8505b9b5
Judith Ríos <165655155+DidithJim@users.noreply.github.com>
2025-02-12 01:39:36 +0100

    Update ContactMe.jsx

--- DidithJim/src/components/ContactMe.jsx
+++ DidithJim/src/components/ContactMe.jsx
@@ -1,21 +1,30 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { MenuContext } from '../components/MenuContext'
 import '../styles/contact.css'
 
-function Contact() {
+function ContactMe() {
+    const { language } = useContext(MenuContext);
+
     return (
         <div className="contact__page">
             <main className='contact__container'>
                 <section className='contact__section'>
-                    <h1 className='contact__title'>Contact</h1>
+                    <h1 className='contact__title'>
+                        {language === 'EN' ? "Contact" : "Contacto"}
+                    </h1>
                     <div className='contact__content'>
                         <div className='contact__group'>
-                            <h2 className='contact__subtitle'>Mail</h2>
+                            <h2 className='contact__subtitle'>
+                                {language === 'EN' ? "Mail" : "Correo"}
+                            </h2>
                             <div className='contact__links'>
                                 <a href="mailto:judithfullstack@gmail.com" className='contact__link'>judithfullstack@gmail.com</a>
                             </div>
                         </div>
                         <div className='contact__group'>
-                            <h2 className='contact__subtitle'>Social Medias</h2>
+                            <h2 className='contact__subtitle'>
+                                {language === 'EN' ? "Social Media" : "Redes Sociales"}
+                            </h2>
                             <div className='contact__links'>
                                 <a href="https://www.linkedin.com/in/judith-r%C3%ADos/" className='contact__link' target="_blank" rel="noreferrer">linkedin</a>
                                 <a href="https://github.com/DidithJim" className='contact__link' target="_blank" rel="noreferrer">github</a>
@@ -28,4 +37,4 @@ function Contact() {
     )
 }
 
-export default Contact;
+export default ContactMe;
